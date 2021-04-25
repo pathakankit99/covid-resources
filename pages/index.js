9fd2433fae89abca4bf99973c07dc0bc47d32dfb
Ankit Pathak <ankitpathak143192@gmail.com>
2021-04-25 12:45:04 +0530

    removed my number

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </main>
 
       
-      <h3 className="bg-blue-700 text-white px-2 py-3 w-full ">Note: Share the info on the website and contribute to the site. Contact Ankit Pathak through email: ankitpathak143192@gmail.com or <a href="tel:7031785903">Click here to call</a> <a target="_blank" href="https://instagram.com/pathakankit99">..</a> for website related problem</h3>
+      <h3 className="bg-blue-700 text-white px-2 py-3 w-full ">Note: Share the info on the website and contribute to the site. Contact Ankit Pathak through email: ankitpathak143192@gmail.com <a target="_blank" href="https://instagram.com/pathakankit99">..</a> for website related problem</h3>
     </div>
   )
 }
